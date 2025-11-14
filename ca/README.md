0b493e35395451158302f314db4883219c916845
Rolly Maulana Awangga <awangga@gmail.com>
2025-11-14 08:21:56 +0700

    hapus cookies.log

---
+++ ca/README.md
@@ -0,0 +1,7 @@
+# LAN RootCA
+
+## Windows Install
+
+```sh
+certutil -addstore -f "ROOT" root-ca.crt
+```
--- ca/main.go
+++ ca/main.go
@@ -24,7 +24,7 @@ func main() {
 	template := x509.Certificate{
 		SerialNumber: big.NewInt(1),
 		Subject: pkix.Name{
-			Organization:  []string{"MiniMax Cookie Sniffer CA"},
+			Organization:  []string{"PT. Mencari Cinta Sejati CA"},
 			Country:       []string{"ID"},
 			Province:      []string{"Jakarta"},
 			Locality:      []string{"Jakarta"},
